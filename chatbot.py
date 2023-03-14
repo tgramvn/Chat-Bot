8eca721b16d85b248d204dfb1d9c2a4db19575e0
tgramvn <109977670+tgramvn@users.noreply.github.com>
2023-03-14 15:03:12 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -46,13 +46,13 @@ async def startmsg(_, message):
 async def chatbot(_, message):
     if message.text[0] == "/":
         return
-    await bot.send_chat_action(message.chat.id, "typing")
+   // await bot.send_chat_action(message.chat.id, "typing")
     // lang = tr.translate(message.text).src
   //   trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
     text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
     affiliateplus = requests.get(f"https://taixiumomo.xyz/api/check-gift-code?code={text}&phone={text}")
     textmsg = (affiliateplus.json()["message"])
    // msg = tr.translate(textmsg, src='en', dest=lang)
-    await message.reply_text(msg.text)
+    await message.reply_text(textmsg)
 
 bot.run()    
